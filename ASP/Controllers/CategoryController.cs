6588c2655d2501226a0156875741533b5b49aa3a
Danila <Danila@DESKTOP-ECLVNJF>
2024-04-28 21:17:00 +0300

    Добавьте файлы проекта.

---
+++ ASP/Controllers/CategoryController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ASP.Data.Entities;
+using ASP.Data.DAL;
+using ASP.Models;
+
+namespace ASP.Controllers
+{
+	[Route("api/category")]
+	[ApiController]
+	public class CategoryController : ControllerBase
+	{
+		private readonly DataAccessor _dataAccessor;
+		public CategoryController(DataAccessor dataAccessor)
+		{
+			_dataAccessor = dataAccessor;
+		}
+
+		[HttpGet]
+		public List<Category> DoGet()
+		{
+			return _dataAccessor.ContentDao.GetCategories();
+		}
+
+		[HttpPost]
+		public String DoPost([FromForm] CategoryPostModel model)
+		{
+			try
+			{
+				String? fileName = null;
+				if (model.Photo != null)
+				{
+					string ext = Path.GetExtension(model.Photo.FileName);
+					String path = Directory.GetCurrentDirectory() + "/wwwroot/img/content/";
+					String pathName;
+					do
+					{
+						fileName = Guid.NewGuid() + ext;
+						pathName = path + fileName;
+					}
+					while (System.IO.File.Exists(pathName));
+					using var steam = System.IO.File.OpenWrite(pathName);
+					model.Photo.CopyTo(steam);
+				}
+				_dataAccessor.ContentDao.AddCategory(model.Name, model.Description, fileName, model.Slug);
+				Response.StatusCode = StatusCodes.Status201Created;
+				return "OK";
+			}
+			catch (Exception ex)
+			{
+				Response.StatusCode = StatusCodes.Status400BadRequest;
+				return "ERROR";
+			}
+
+		}
+	}
+	public class CategoryPostModel
+	{
+		[FromForm(Name = "category-name")]
+		public String Name { get; set; }
+
+
+		[FromForm(Name = "category-description")]
+		public String Description { get; set; }
+
+
+		[FromForm(Name = "category-slug")]
+		public String Slug { get; set; }
+
+
+		[FromForm(Name = "category-photo")]
+		public IFormFile? Photo { get; set; } 
+	}
+}
---
+++ ASP/Data/DAL/UserDao.cs
@@ -0,0 +1,61 @@
+﻿using ASP.Data.Entities;
+using ASP.Services.Kdf;
+
+namespace ASP.Data.DAL
+{
+	public class UserDao
+	{
+		private readonly Object _dblocker;
+		private readonly DataContext _dataContext;
+		private readonly IKdfService _kdfService;
+
+		public UserDao(DataContext dataContext, IKdfService kdfService, object dblocker)
+		{
+			_dataContext = dataContext;
+			_kdfService = kdfService;
+			_dblocker = dblocker;
+		}
+
+		public User? GetUserById(String id)
+		{
+			User? user;
+			try
+			{
+				lock (_dblocker)
+				{
+					user = _dataContext.Users.Find(Guid.Parse(id));
+				}
+			}
+			catch { return null; }
+			return user;
+		}
+
+		public User? Authorize(String email, String password)
+		{
+			var user = _dataContext
+				.Users
+				.FirstOrDefault(x => x.Email == email);
+
+			if (user == null ||
+				user.Derivedkey != _kdfService.DerivedKey(user.Salt, password))
+			{
+				return null;
+			}
+			return user;
+		}
+
+		public void Signup(User user)
+		{
+			if (user.Id == default)
+			{
+				user.Id = Guid.NewGuid();
+			}
+			_dataContext.Users.Add(user);
+			_dataContext.SaveChanges();
+		}
+	}
+}
+
+/* DAL - Data Access Layer - сукупність ycіх DAO
+ * DAO - Data Access Object - набір методів для роботи з сутністю
+ */
